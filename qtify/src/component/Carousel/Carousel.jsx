f52e0bc5e06ebb2b9536b7d08c5fcf8aa9af975b
RammeharChahar <rammeharchahar123@gmail.com>
2023-12-22 09:18:18 +0530

    album section commit

---
+++ qtify/src/component/Carousel/Carousel.jsx
@@ -0,0 +1,62 @@
+import React from "react";
+import styles from "./Carousel.module.css";
+import { Swiper, SwiperSlide, useSwiper } from "swiper/react";
+import { Navigation } from "swiper/modules";
+import "swiper/css/navigation";
+import "swiper/css";
+import Card from "../Card/Card";
+import { useEffect, useRef } from "react";
+import leftIcon from "../../assets/left-slide.svg";
+import rightIcon from "../../assets/right-slide.svg";
+
+function Carousel({ data2 }) {
+  const swiperRef = useRef(null);
+  const swiper = useSwiper();
+  const CustomPrevButton = () => (
+    <button
+      className={styles.prevButton}
+      onClick={() => swiperRef.current.swiper.slidePrev()}
+    >
+      <img src={leftIcon}></img>
+    </button>
+  );
+  const CustomNextButton = () => (
+    <button
+      className={styles.nextButton}
+      onClick={() => swiperRef.current.swiper.slideNext()}
+    >
+      <img src={rightIcon}></img>
+    </button>
+  );
+
+  return (
+    <>
+      <Swiper
+        ref={swiperRef}
+        spaceBetween={15}
+        slidesPerView={7}
+        onReachEnd={() => {
+          console.log("slide ended");
+        }}
+        onSwiper={(swiper) => console.log(swiper)}
+        modules={[Navigation]}
+        navigation={{
+          prevEl: ".custom-prev-button",
+          nextEl: ".custom-next-button",
+        }}
+      >
+        {data2.map((ele) => (
+          <SwiperSlide>
+            <Card data1={ele} />
+          </SwiperSlide>
+        ))}
+        <div className={styles.customContainer}>
+          <CustomPrevButton />
+          <CustomNextButton />
+        </div>
+      </Swiper>
+    </>
+  );
+}
+
+export default Carousel;
--- qtify/src/component/Section/Section.js
+++ qtify/src/component/Section/Section.js
@@ -1,19 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from "./Section.module.css";
 import Button from '@mui/material/Button';
 import Card from '../Card/Card';
+import Carousel from '../Carousel/Carousel';
 
 function Section({title,data,type}) {
+   const [toggleBtn,setToggleBtn] = useState(true);
+   const [toggleText,setToggleText] = useState('Collapse');
+
+   const handleToggle = () =>{
+    if(toggleBtn){
+      setToggleText('Show All');
+    }else{
+      setToggleText('Collapse');
+    }
+    setToggleBtn(!toggleBtn);
+   }
+
   return (
     <div className={styles.albumWrapper}>
-        <div className={styles.title}>
-            <p className={styles.heading}>{title}</p>
-            <Button variant="text" className={styles.button1}>Collapse</Button>
-        </div>
-        <div className={styles.albumcards}>
-        {data.map((item) => <Card data1={item}/>)}
-        </div>
+    <div className={styles.title}>
+        <p className={styles.heading}>{title}</p>
+        <Button variant="text" className={styles.button1} onClick={handleToggle}>{toggleText}</Button>
     </div>
+    <div className={styles.albumcards}>
+     { toggleBtn ?  data.map((item) => <Card data1={item}/>) 
+       : <Carousel data2={data}/>}
+    </div>
+</div>
   )
 }
 
